0043d01435c2ee264c007f0caf4bc96c120ad9ed
Ivanna86Koval <ms.ivanna86@gmail.com>
2023-09-21 23:28:08 +0300

    update RootStyle.styled.js

--- src/components/RootStyle/RootStyle.styled.js
+++ src/components/RootStyle/RootStyle.styled.js
@@ -4,12 +4,18 @@ import 'modern-normalize';
 export const RootStyle = createGlobalStyle`
 body {
   margin: 0;
-  background-color: aqua;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  background: linear-gradient(90deg,#4cd964,#5ac8fa,#007aff,#34aadc,#5856d6,#ff2d55);
+    transition: opacity 250ms linear;
+}
+
+.body:hover::before {
+  opacity: 1;
+ //transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
   }
 
 code {
